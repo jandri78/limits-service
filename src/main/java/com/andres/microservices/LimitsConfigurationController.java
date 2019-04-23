c82c31efebd94be6805034c30097ff0538d0d55a
Andres Perez <jandri78@gmail.com>
2019-04-23 15:38:34 -0500

    zipkin   & amqp , hystrix , management.endpoints.web.exposure.include=*

--- src/main/java/com/andres/microservices/LimitsConfigurationController.java
+++ src/main/java/com/andres/microservices/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
 	
@@ -15,4 +17,20 @@ public class LimitsConfigurationController {
 	
 		return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
 	}
+	
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetriveConfigurations")
+	public LimitsConfiguration retriveConfigurations() {
+	
+		throw new RuntimeException("Not avaiable");
+	}
+	
+	
+	public LimitsConfiguration fallbackRetriveConfigurations()
+	{
+		return new LimitsConfiguration(9999,99);
+		
+	}	
+	
 }
